0fc3f64b4bbd0d0037936d9c8c33ebae14df2178
NikoJingyuHuang <38271309+NikoJingyuHuang@users.noreply.github.com>
2019-06-10 16:47:05 +0100

    UI adapt

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -24,6 +24,7 @@ public class UIController : MonoBehaviour
 
     private void Start()
     {
+        
         startButton.onClick.AddListener(OnStartButtonClick);
         exitButtion.onClick.AddListener(OnExitButtionClick);
     }
