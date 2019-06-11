5df60a62f7d4b2de834c78e6ea43cf9a5e75ada6
Leon <962942172@qq.com>
2019-06-11 13:11:48 +0100

    Add animation scripts

--- Assets/Scripts/Animation/Animations.cs
+++ Assets/Scripts/Animation/Animations.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DragonBones;
+
+[System.Serializable]
+public class Animation
+{
+    public string name;  //动画名称，Animation name
+    public float timeScale; //动画播放速度 The speed of playing animation
+    public float totalTime; //动画播放的总时间 The total playing time of current animation
+    public bool isCompleted; //动画是否播放完成 Is animation completed
+    public bool isStart; //动画是否已经开始 Is animation started
+    public bool canChange; //动画当前是否可以改变 Can animation change
+}
 
 public class Animations : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int sizeOfAnimationsClass;
+    public Animation[] animations;
+    public UnityArmatureComponent _amature;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        animations = new Animation[_amature.animation.animationNames.Count];
+        print(sizeOfAnimationsClass);
     }
 }
